878233602c4031b99e782b3dc8ed676b9353bc6e
Trey Baxter <treywbaxter@gmail.com>
2025-07-03 19:24:07 -0700

    Fix cert download script & syntax in main.yml

--- roles/configure-firefox/files/getburpcert.sh
+++ roles/configure-firefox/files/getburpcert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 burp=$(find / -name burp*.jar 2>/dev/null | tail -1)
-/bin/bash -c "timeout 45 /usr/share/burpsuite/jre/bin/java -Djava.awt.headless=true -jar $burp < <(echo y) &"
+java_cmd=$(which java || echo "/usr/bin/java")
+timeout 45 "$java_cmd" -Djava.awt.headless=true -jar "$burp" < <(echo y) &
 sleep 30
-curl http://127.0.0.1:8080/cert -o /tmp/cacert.der
+curl http://localhost:8080/cert -o /tmp/cacert.der
 exit
